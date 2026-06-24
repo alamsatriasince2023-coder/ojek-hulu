78ab91d7d97dac8e39c3902d414531eb2650536c
alamsatriasince2023-coder <alamsatria.since2023@gmail.com>
2026-06-24 19:47:16 +0700

    Update auth.js

--- ojek-hulu/js/auth.js
+++ ojek-hulu/js/auth.js
@@ -1,5 +1,68 @@
 import { supabase } from './api.js';
 
-export async function loginUser(email, password) {
-    return await supabase.auth.signInWithPassword({ email, password });
+export async function registerUser(
+email,
+password,
+meta
+){
+
+return await supabase.auth.signUp({
+
+email,
+password,
+
+options:{
+data:meta
+}
+
+});
+
+}
+
+export async function loginUser(
+email,
+password
+){
+
+return await supabase.auth.signInWithPassword({
+
+email,
+password
+
+});
+
+}
+
+export async function logoutUser(){
+
+await supabase.auth.signOut();
+
+}
+
+export async function getCurrentUser(){
+
+const {
+data:{user}
+}
+=
+await supabase.auth.getUser();
+
+return user;
+
+}
+
+export async function getProfile(){
+
+const user = await getCurrentUser();
+
+if(!user) return null;
+
+const { data } = await supabase
+.from('profiles')
+.select('*')
+.eq('id', user.id)
+.single();
+
+return data;
+
 }
