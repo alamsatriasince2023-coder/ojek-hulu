46669a77b243453488a7de26b562084ccf10465b
alamsatriasince2023-coder <alamsatria.since2023@gmail.com>
2026-06-24 19:07:49 +0700

    Update map.js

--- ojek-hulu/js/map.js
+++ ojek-hulu/js/map.js
@@ -1,6 +1,20 @@
 export function initMap() {
     const map = L.map('map').setView([0.8443, 112.9234], 14);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-    const marker = L.marker([0.8443, 112.9234], { draggable: true }).addTo(map);
-    return marker;
+
+    L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        {
+            attribution: '&copy; OpenStreetMap'
+        }
+    ).addTo(map);
+
+    const marker = L.marker(
+        [0.8443, 112.9234],
+        { draggable: true }
+    ).addTo(map);
+
+    return {
+        map,
+        marker
+    };
 }
